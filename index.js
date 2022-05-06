6dcb9afb164a9692cdd1227e0b4c1aee1f230b00
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-06 17:12:07 +0600

    all data found

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,29 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p3ee6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const bookCollection = client.db('booksWarehouse').collection('items');
+
+        app.get('/items', async(req, res)=>{
+            const query = {};
+            const cursor = bookCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
     res.send('Server is running');
