9466a96cb98df17f6752dd039fe420b26b527197
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-07 11:06:03 +0600

    find api added

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run(){
             const item = await bookCollection.findOne(query);
             res.send(item);
         });
+
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await bookCollection.deleteOne(query);
+            res.send(item);
+        });
     }
     finally{
 
