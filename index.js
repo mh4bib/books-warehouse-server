fec5987da19d01202570930f28edc702030ff3e8
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-08 23:42:50 +0600

    final

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,16 +11,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeaders.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p3ee6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const bookCollection = client.db('booksWarehouse').collection('items');
 
-        app.get('/items', async(req, res)=>{
+        //jwt
+        app.post('/token', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '7d'
+            });
+            res.send({ accessToken });
+        });
+
+
+        app.get('/items', async (req, res) => {
             const query = {};
             const cursor = bookCollection.find(query);
             const items = await cursor.toArray();
@@ -28,7 +54,6 @@ async function run(){
 
         app.get('/items/:id', async (req, res) => {
             const id = req.params.id;
-            // console.log(id);
             const query = { _id: ObjectId(id) };
             const item = await bookCollection.findOne(query);
             res.send(item);
@@ -47,40 +72,46 @@ async function run(){
             res.send(item);
         });
 
-        app.get('/my-item', async (req, res) => {
+        app.get('/my-item', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = bookCollection.find(query);
-            const item = await cursor.toArray();
-            res.send(item);
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = bookCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
         });
 
-        app.put('/items/:id', async (req, res) =>{
+        app.put('/items/:id', async (req, res) => {
             const id = req.params.id;
             const updatedQuantity = req.body;
-            console.log(updatedQuantity.newQuantity);
             const filter = { _id: ObjectId(id) };
-            const options = {upsert:true};
+            const options = { upsert: true };
             const updatedDoc = {
                 $set: {
-                    quantity:updatedQuantity.newQuantity
+                    quantity: updatedQuantity.newQuantity
                 }
             };
             const result = await bookCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         });
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening to port', port);
 });
